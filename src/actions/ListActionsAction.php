d0635a27e8adfa14f7b704a76f83871859ef58a5
TJ Draper <tj@buzzingpixel.com>
2019-01-05 14:49:47 -0600

    Add copyright notices to all PHP files

--- src/actions/ListActionsAction.php
+++ src/actions/ListActionsAction.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2019 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
 namespace corbomite\cli\actions;
 
 use Symfony\Component\Console\Output\OutputInterface;
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2019 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
