fc3e507560d614deadee3cabf13b16141983f638
TJ Draper <tj@buzzingpixel.com>
2019-02-24 16:27:42 -0600

    Set up phpunit testing in docker container

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * @author TJ Draper <tj@buzzingpixel.com>
- * @copyright 2019 BuzzingPixel, LLC
- * @license Apache-2.0
- */
+define('TESTS_BASE_PATH', __DIR__);
+
+require dirname(__DIR__) . '/vendor/autoload.php';
